ef20ccaa6c51d15c93114c8c9e02ddea94b94fb0
conguyen <conguyen.270601@gmail.com>
2024-06-19 00:08:12 +0700

    fix: 5

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,7 @@ ssh -i $SERVER_SSH_KEY_FILE -o StrictHostKeyChecking=no ubuntu@ec2-3-1-6-106.ap-
   cd cicd
   git pull
   cd ../
-  export PATH=$PATH:/usr/local/bin
+  export PATH="$PATH:/home/ubuntu/.nvm/versions/node/v20.14.0/bin"
+  export PATH="$PATH:/.nvm/versions/node/v20.14.0/bin"
   pm2 restart server
 '
